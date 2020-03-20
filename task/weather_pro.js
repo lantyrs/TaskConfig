ee6126d71c8f42822b2270d5e1a999143f7ff328
Jason <19920823Liu>
2020-03-20 19:42:35 +0800

    weather _pro

--- task/weather_pro.js
+++ task/weather_pro.js
@@ -15,16 +15,17 @@ let config = {
         template: {
             title: `[天气日报] $[city] $[summary]`,
             subtitle: `$[weatherIcon]$[weather] $[temperatureMin] ~ $[temperatureMax]°C ☔️降雨概率 $[precipProbability]%`,
-            detail: `🥵空气质量 $[aqi]($[aqiDesc]) 🍃风速$[windSpeed]km/h $[windDir]
-🌞紫外线指数 $[uv]($[uvDesc]) 💧湿度$[currentHumidity]%
-🌡体感温度 $[apparentTemperatureMin] ~ $[apparentTemperatureMax]°C 💨气压$[atmosphere]pa
+            detail: `🌡体感温度 $[apparentTemperatureMin] ~ $[apparentTemperatureMax]°C 
+                    🥵空气质量 $[aqi]($[aqiDesc]) 🍃风速$[windSpeed]km/h $[windDir]
+                    🌞紫外线指数 $[uv]($[uvDesc]) 💧湿度$[currentHumidity]%
+                    💨气压$[atmosphere]pa
 
-[生活指数]
-$[lifeStyle($[icon][$[brf]]$[txt])]
+                    [生活指数]
+                    $[lifeStyle($[icon][$[brf]]$[txt])]
 
-[天气周报]
-$[daily($[month]月$[day]日  $[temperatureLow]~$[temperatureHigh]°C  $[weatherIcon]$[weather])]`
-        },
+                    [天气周报]
+                    $[daily($[month]月$[day]日  $[temperatureLow]~$[temperatureHigh]°C  $[weatherIcon]$[weather])]`
+                    },      
         lifestyle: { //此处用于显示各项生活指数，可自行调整顺序，顺序越在前面则显示也会靠前，如果您不想查看某一指数，置为false即可，想看置为true即可
             drsg: true, //穿衣指数,
             flu: true, //感冒指数,
