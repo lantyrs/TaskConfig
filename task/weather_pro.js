0943f4c1173e36952d379b37dd9cdd3626110389
Jason <19920823Liu>
2020-03-20 19:58:38 +0800

    Update weather_pro.js

--- task/weather_pro.js
+++ task/weather_pro.js
@@ -18,7 +18,7 @@ let config = {
             // detail: `🌡体感温度 $[apparentTemperatureMin] ~ $[apparentTemperatureMax]°C 🥵空气质量 $[aqi]($[aqiDesc]) 🍃风速$[windSpeed]km/h $[windDir] 🌞紫外线指数 $[uv]($[uvDesc]) 💧湿度$[currentHumidity]%
             //         💨气压$[atmosphere]pa
             title: `[天气日报] $[city] $[district] 
-            $[summary]`,
+                    $[summary]`,
             subtitle: `$[weatherIcon]$[weather] $[temperatureMin] ~ $[temperatureMax]°C ☔️降雨概率 $[precipProbability]%`,
             detail: `🥵空气质量 $[aqi]($[aqiDesc]) 🌬风速$[windSpeed]km/h $[windDir]
 🌞紫外线指数 $[uv]($[uvDesc]) 💧湿度$[currentHumidity]%
